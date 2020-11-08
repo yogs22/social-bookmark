74f3fb0f91c231604e921e2003c94cd9ddb2fa6a
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2020-11-08 11:41:44 +0700

    Update content.blade.php

--- resources/views/sitemap/content.blade.php
+++ resources/views/sitemap/content.blade.php
@@ -1,11 +1,11 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>'."\n"; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   @foreach ($contents as $content)
     <url>
       <loc>{{ route('home.single', $content->slug) }}</loc>
       <lastmod>{{ $content->created_at->toAtomString() }}</lastmod>
-      <changefreq>weekly</changefreq>
-      <priority>0.6</priority>
+      <changefreq>daily</changefreq>
+      <priority>0.8</priority>
     </url>
   @endforeach
 </urlset>
